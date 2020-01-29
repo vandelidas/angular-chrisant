53b32407f22e23d96a4f567fe1ad84856dc7fef6
vandelidas <ernelgado@gmail.com>
2020-01-29 08:29:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-chrisant
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-chrisant)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'IPhone XL',
+    price: 1799,
+    description: 'Un excelente equipo el cual considerar'
+  },
+  {
+    name: 'Samsung S10',
+    price: 1699,
+    description: 'Un excelente equipo el cual considerar'
+  },
+  {
+    name: 'Phone Standard',
+    price: 1299,
+    description: 'Un excelente equipo el cual considerar'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
